22bcec36ee7dcf4fc350075acca82df85e64250d
g-libardi <contato@libardi.dev.br>
2024-11-16 19:20:20 -0300

    fix(fe): remove unused component

--- frontend/src/shared/main.tsx
+++ frontend/src/shared/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './main.css'
 import DefaultLayout from './layouts/default'
-import { Button } from './components/ui/button'
 import Dashboard from './pages/dashboard'
 
 createRoot(document.getElementById('root')!).render(

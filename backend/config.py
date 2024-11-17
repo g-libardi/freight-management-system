71687f220db4eacdee285fe233277f8577849e08
g-libardi <contato@libardi.dev.br>
2024-11-17 19:38:08 -0300

    feat(be): define server config

---
+++ backend/config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_DSN = os.getenv("DB_DSN", "postgresql://user:password@localhost:5432/db")
+MODE = os.getenv("MODE", "DEV").upper()
+DEV = MODE == "DEV"

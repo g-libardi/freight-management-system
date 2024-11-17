7c447dced19707e6db36bc495a73e308adff9a4e
g-libardi <contato@libardi.dev.br>
2024-11-17 18:38:33 -0300

    feat(fe): add DataView component for freights

---
+++ frontend/src/freight/components/data/freight-table.tsx
@@ -0,0 +1,40 @@
+import Freight from "@/freight/entities/freight";
+import useFreightData from "@/freight/hooks/use-freight-data";
+import TableView from "@/shared/components/data/table-view";
+import { useMemo } from "react";
+
+
+const header: { key: keyof Freight; label: string }[] = [
+  { key: "id", label: "Número" },
+  { key: "date", label: "Data" },
+  { key: "cargo_type", label: "Tipo de Carga" },
+  { key: "carrier", label: "Transportadora/Carregadora" },
+  { key: "freight_status", label: "Status" },
+  { key: "price", label: "Custo total (R$)" },
+];
+
+
+
+
+export default function FreightTable() {
+  const dataService = useFreightData()
+  
+  function mapper(item: Freight) {
+    return {
+      ...item,
+      date: new Date(item.date).toLocaleDateString(),
+      driver: item.driver.name,
+      carrier: item.carrier.name,
+      cargo_type: item.cargo_type.name,
+      freight_status: item.freight_status.name,
+    }
+  }
+
+  function doSomething(item: Freight) {
+    alert(`Finalizando frete ${item.id}`)
+  }
+
+  return (
+    <TableView data={dataService.data} setData={dataService.setData} header={header} dataMapper={mapper} actions={[{label: "Finalizar", onClick: doSomething}]} />
+  )
+}
--- frontend/src/shared/pages/dashboard.tsx
+++ frontend/src/shared/pages/dashboard.tsx
@@ -1,9 +1,13 @@
+import FreightTable from "@/freight/components/data/freight-table";
+import { Card } from "../components/ui/card";
 
 
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="flex flex-col items-center justify-center size-full p-14">
+      <Card className="size-full p-3">
+        <FreightTable />
+      </Card>
     </div>
   )
 }

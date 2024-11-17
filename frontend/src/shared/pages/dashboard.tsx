3d6ddc8c1dd7b4db219531ec094eb25f9dbd384d
g-libardi <contato@libardi.dev.br>
2024-11-17 18:55:44 -0300

    style(fe): make freihts table contained by scroll

--- frontend/src/shared/pages/dashboard.tsx
+++ frontend/src/shared/pages/dashboard.tsx
@@ -4,8 +4,8 @@ import { Card } from "../components/ui/card";
 
 export default function Dashboard() {
   return (
-    <div className="flex flex-col items-center justify-center size-full p-14">
-      <Card className="size-full p-3">
+    <div className="flex flex-col items-center justify-center size-full max-h-dvh p-14">
+      <Card className="size-full p-3 max-h-full overflow-y-auto">
         <FreightTable />
       </Card>
     </div>

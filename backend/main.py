033ae4010db4d6bf3918a863cc803abb7be9f082
g-libardi <contato@libardi.dev.br>
2024-11-17 19:43:23 -0300

    feat(be): add general setup to main file

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,26 @@
 from fastapi import FastAPI
+import data
+from contextlib import asynccontextmanager
+from modules import router
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app):
+    '''Initializes the database connection pool.'''
+    async with data.init_db():
+        yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router, prefix='/api')
+
+
+# Allow CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
